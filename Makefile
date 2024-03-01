4421e5426028c0212f8d8bffa04bf9d5f5bbbf99
dantol29 <dantol29@gmail.com>
2024-03-01 15:29:22 +0100

    MLX added

--- Makefile
+++ Makefile
@@ -1,17 +1,26 @@
 NAME = cub3D
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
+LIBMLX	:= ./MLX42
+HEADERS	:= -I ./include -I $(LIBMLX)/include
+LIBS	:= $(LIBMLX)/build/libmlx42.a -ldl -lglfw -pthread -lm
 LIBFT = libft
 SANITAZER =  -fsanitize=address
 SOURCES = src/main.c \
 
 OBJECTS = $(SOURCES:.c=.o)
 
-all: $(NAME)
+all: libmlx $(NAME)
+
+libmlx:
+	@cmake $(LIBMLX) -B $(LIBMLX)/build && make -C $(LIBMLX)/build -j4
+
+%.o: %.c
+	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS) && printf "Compiling: $(notdir $<)"
 
 $(NAME) : $(OBJECTS)
 		@make -C $(LIBFT)
-		$(CC) $(CFLAGS) $(OBJECTS) -L./$(LIBFT) -lft $(SANITAZER) -o $(NAME)
+		$(CC) $(CFLAGS) $(OBJECTS) -L./$(LIBFT) -lft $(LIBS) $(HEADERS) $(SANITAZER) -o $(NAME)
 
 clean:
 	rm -rf $(OBJECTS)
--- src/main.c
+++ src/main.c
@@ -3,17 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akurmyza <akurmyza@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 12:44:22 by akurmyza          #+#    #+#             */
-/*   Updated: 2024/03/01 12:47:03 by akurmyza         ###   ########.fr       */
+/*   Updated: 2024/03/01 15:08:36 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
-int	main(int argc, char **argv)
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 1920
+#define HEIGHT 960
+
+static void ft_error(void)
 {
-	
-	return (0);
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+static void ft_hook(void* param)
+{
+	const mlx_t* mlx = param;
+
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+}
+
+int32_t	main(void)
+{
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	if (!mlx)
+		ft_error();
+	mlx_image_t* img = mlx_new_image(mlx, 1920, 960);
+
+    // Set the channels of each pixel in our image to the maximum byte value of 255. 
+    ft_memset(img->pixels, 200, img->width * img->height * 4);
+
+    // Draw the image at coordinate (0, 0).
+    mlx_image_to_window(mlx, img, 0, 0);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
