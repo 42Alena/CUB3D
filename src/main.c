b959106303118051527f754c71faf74297d6deba
dantol29 <dantol29@gmail.com>
2024-03-04 18:59:39 +0100

    added start of textured raycasting

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 12:44:22 by akurmyza          #+#    #+#             */
-/*   Updated: 2024/03/04 18:17:46 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/03/04 18:59:07 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,6 @@
 // 	mlx_put_pixel(game->image, i, j, 0x36CC89FF);
 // }
 
-// double wallX; //where exactly the wall was hit
-// 		if (side == 0)
-// 			wallX = game->player_y + perpWallDist * rayDirY;
-// 		else
-// 			wallX = game->player_x + perpWallDist * rayDirX;
-// 		wallX -= floor((wallX));
-// 		int texX = (int)(IMAGE_WIDTH * wallX);
-// 		if(side == 0 && rayDirX > 0)
-// 			texX = IMAGE_WIDTH - texX - 1;
-// 		if(side == 1 && rayDirY < 0)
-// 			texX = IMAGE_WIDTH - texX - 1;
-
 double	distance(int side, double sideDistX, double sideDistY, double deltaDistX, double deltaDistY)
 {
 	if (side == 0)
@@ -47,24 +35,24 @@ double	distance(int side, double sideDistX, double sideDistY, double deltaDistX,
 		return (sideDistY - deltaDistY);
 }
 
-void	draw_wall(t_game *game, double perpWallDist, int i, int color)
-{
-	int	lineHeight;
-	int	drawStart;
-	int	drawEnd;
+// int	draw_wall(t_game *game, double perpWallDist, int i, int color)
+// {
+// 	int	lineHeight;
+// 	int	drawStart;
+// 	int	drawEnd;
 	
-	lineHeight = (int)(WINDOW_HEIGHT / perpWallDist);
-	printf("height:%d\n", lineHeight);
-	drawStart = -lineHeight / 2 + WINDOW_HEIGHT / 2;
-	if (drawStart < 0)
-		drawStart = 0;
-	drawEnd = lineHeight / 2 + WINDOW_HEIGHT / 2;
-	if(drawEnd >= WINDOW_HEIGHT)
-		drawEnd = WINDOW_HEIGHT - 1;
-	for (int j = drawStart; j < drawEnd; j++)
-		mlx_put_pixel(game->image, i, j, 0x36CC89FF);
-	(void)color;
-}
+// 	lineHeight = (int)(WINDOW_HEIGHT / perpWallDist);
+// 	drawStart = -lineHeight / 2 + WINDOW_HEIGHT / 2;
+// 	if (drawStart < 0)
+// 		drawStart = 0;
+// 	drawEnd = lineHeight / 2 + WINDOW_HEIGHT / 2;
+// 	if(drawEnd >= WINDOW_HEIGHT)
+// 		drawEnd = WINDOW_HEIGHT - 1;
+// 	for (int j = drawStart; j < drawEnd; j++)
+// 		mlx_put_pixel(game->image, i, j, 0x36CC89FF);
+// 	(void)color;
+// 	return (lineHeight);
+// }
 //0x36CC89FF
 void	calculate(t_game* game)
 {
@@ -74,7 +62,7 @@ void	calculate(t_game* game)
 						{1,2,0,0,1},\
 						{1,1,1,1,1}};
 	game->image = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	int color = rand () % 0x100000000;
+	//int color = rand () % 0x100000000;
 	for (double i = 0, w = WINDOW_WIDTH; i < w; i++)
 	{
 		int			mapX = (int)game->player_x;
@@ -126,7 +114,39 @@ void	calculate(t_game* game)
 		}
 		perpWallDist = distance(side, sideDistX, sideDistY, deltaDistX, deltaDistY);
 		printf("distance:%f\n", perpWallDist);
-		draw_wall(game, perpWallDist, i, color);
+		int lineHeight = (int)(WINDOW_HEIGHT / perpWallDist);
+		int drawStart = -lineHeight / 2 + WINDOW_HEIGHT / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + WINDOW_HEIGHT / 2;
+		if(drawEnd >= WINDOW_HEIGHT)
+			drawEnd = WINDOW_HEIGHT - 1;
+		for (int j = drawStart; j < drawEnd; j++)
+		{
+			mlx_put_pixel(game->image, i, j, 0x36CC89FF);
+		}
+		// double wallX; //where exactly the wall was hit
+		// if (side == 0)
+		// 	wallX = game->player_y + perpWallDist * rayDirY;
+		// else
+		// 	wallX = game->player_x + perpWallDist * rayDirX;
+		// wallX -= floor((wallX));
+		// int texX = (int)(IMAGE_WIDTH * wallX);
+		// if(side == 0 && rayDirX > 0)
+		// 	texX = IMAGE_WIDTH - texX - 1;
+		// if(side == 1 && rayDirY < 0)
+		// 	texX = IMAGE_WIDTH - texX - 1;
+		// double step = 1.0 * IMAGE_HEIGHT / lineHeight;
+		// // Starting texture coordinate
+		// int texNum = map[mapX][mapY] - 1;
+		// double texPos = (drawStart - pitch - h / 2 + lineHeight / 2) * step;
+		// for(int y = drawStart; y < drawEnd; y++)
+		// {
+		// 	// Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
+		// 	int texY = (int)texPos & (IMAGE_HEIGHT - 1);
+		// 	texPos += step;
+		// 	Uint32 color = texture[texNum][IMAGE_HEIGHT * texY + texX];
+		// }
 	}
 	mlx_image_to_window(game->mlx, game->image, 0, 0);
 }
