222815b3e1b8442e110a8f54532e26a21573be2e
dantol29 <dantol29@gmail.com>
2024-03-05 15:24:05 +0100

    raycasting with generated textures

--- src/game.c
+++ src/game.c
@@ -6,7 +6,7 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 22:32:28 by akurmyza          #+#    #+#             */
-/*   Updated: 2024/03/05 14:06:28 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:05:22 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,19 @@ void init_game_struct(t_game *game)
 	for (int x = 0; x < IMAGE_WIDTH; x++)
 	{
 		for (int y = 0; y < IMAGE_HEIGHT; y++)
+		{
+			int xorcolor = (x * 256 / IMAGE_WIDTH) ^ (y * 256 / IMAGE_HEIGHT);
+			int ycolor = y * 256 / IMAGE_HEIGHT;
+			int xycolor = y * 128 / IMAGE_HEIGHT + x * 128 / IMAGE_WIDTH;
 			game->texture[0][IMAGE_WIDTH * y + x] = 65536 * 254 * (x != y && x != IMAGE_WIDTH - y);
+			game->texture[1][IMAGE_WIDTH * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
+			game->texture[2][IMAGE_WIDTH * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
+			game->texture[3][IMAGE_WIDTH * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
+			game->texture[4][IMAGE_WIDTH * y + x] = 256 * xorcolor; //xor green
+			game->texture[5][IMAGE_WIDTH * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
+			game->texture[6][IMAGE_WIDTH * y + x] = 65536 * ycolor; //red gradient
+			game->texture[7][IMAGE_WIDTH * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
+		}
 	}
 	game->player.player_x = 2.5;
 	game->player.player_y = 2.5;
--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: dtolmaco <dtolmaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 13:06:47 by dtolmaco          #+#    #+#             */
-/*   Updated: 2024/03/05 14:30:22 by dtolmaco         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:20:31 by dtolmaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,34 +100,36 @@ void	calculate_start_end(t_game *game)
 	game->ray.step = 1.0 * IMAGE_HEIGHT / game->ray.line_height;
 }
 
+int get_rgba(int r, int g, int b, int a)
+{
+    return (r << 24 | g << 16 | b << 8 | a);
+}
+
 void	draw(t_game *game, int i)
 {
-	int pitch = 100;
-	int	color;
-	double texPos = (game->ray.draw_start - pitch - WINDOW_HEIGHT / 2\
+	int 		texY;
+	int			tex_num;
+	uint32_t	color;
+	
+	tex_num = 6;
+	if (game->ray.side == 0)
+		tex_num = 5;
+	double texPos = (game->ray.draw_start - WINDOW_HEIGHT / 2\
 	+ game->ray.line_height / 2) * game->ray.step;
 	for(int y = game->ray.draw_start; y < game->ray.draw_end; y++)
 	{
-		int texY = (int)texPos & (IMAGE_HEIGHT - 1);
+		texY = (int)texPos & (IMAGE_HEIGHT - 1);
 		texPos += game->ray.step;
-		color = game->texture[0][IMAGE_HEIGHT * texY + game->ray.tex_x];
-		mlx_put_pixel(game->image, i, y, 0x36CC89FF);
+		color = game->texture[tex_num][IMAGE_HEIGHT * texY + game->ray.tex_x];
+		int red = (color >> 16) & 0xFF;
+		int green = (color >> 8) & 0xFF;
+		int blue = color & 0xFF;
+		mlx_put_pixel(game->image, i, y, get_rgba(red, green, blue, 255));
 	}
 }
 
 void	raycasting(t_game* game)
 {
-	// char	map[50][50] = {{1,1,1,1,1},\
-	// 					{1,0,0,0,1},\
-	// 					{1,0,0,0,1},\
-	// 					{1,0,0,0,1},\
-	// 					{1,1,0,1,1},
-	// 					{1,0,0,0,1},
-	// 					{1,1,0,1,1},
-	// 					{1,1,0,1,1},
-	// 					{1,1,0,1,1},
-	// 					{1,0,0,0,1},
-	// 					{1,1,1,1,1}};
 	game->image = mlx_new_image(game->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	for (int i = 0, w = WINDOW_WIDTH; i < w; i++)
 	{
